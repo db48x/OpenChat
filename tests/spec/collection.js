0df003c8ee65b3f54d2713e49af1cbba8646e833
Peter Moser <peter.moser@gmail.com>
2013-09-11 17:14:01 -0700

    add tests for add and remove methods of the collection class

--- tests/spec/collection.js
+++ tests/spec/collection.js
@@ -1,14 +1,29 @@
 describe("the Collection class",
          function () {
+             var c;
+
              beforeEach(function () {
                  spyOn(window, 'Collection').andCallThrough();
+                 c = new Collection();
              });
 
              it("should not throw in the constructor",
                 function () {
-                    var c = new Collection();
                     expect(c).toBeTruthy();
                     expect(Collection).toHaveBeenCalled();
                     expect(Collection).not.toThrow();
+                });
+
+             it("should remember things that are added",
+                function () {
+                    c.add("foo", 42);
+                    expect(c.item("foo")).toBe(42);
+                });
+
+             it("should forget things that are removed",
+                function () {
+                    c.add("foo", 42);
+                    c.remove("foo");
+                    expect(c.item("foo")).toBe(undefined);
                 });
          });
